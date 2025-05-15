5f18dcd00770c73e837e700802140f42cfaaae7f
Steve Baynes <baynescorps@gmail.com>
2025-05-15 12:06:41 +0100

    Set up basic layout of both products list and product pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export async function getStaticProps<ProductsProps>(context) {
+export async function getStaticProps<ProductsProps>(context: any) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL || ''}`)
   const products= await res.json()
 
@@ -30,12 +30,16 @@ export async function getStaticProps<ProductsProps>(context) {
 
 export default function Home({products}: ProductsProps) {
 
+  //TODO: extract this out to a component
   const productslist = products.map(({id, brand, category, description, images, price, stock, thumbnail, title}:ProductProps)=>{
     return (
-      <div>
-        <Image src={thumbnail} alt={title} width="150" height="150"/>
-        <div><Link href={`/product/${id}`}>{title}</Link></div>
-        <div>£{price}</div>
+      <div className="border rounded-lg p-4">
+        <div className="justify-items-center">
+          <Image src={thumbnail} alt={title} width="150" height="150"/>
+        </div>
+        <div><Link className="underline hover:font-bold" href={`/product/${id}`}>{title}</Link></div>
+        <div className="font-bold text-sm capitalize">{category}</div>
+        <div className="font-bold">£{price}</div>
       </div>
     )
   })
@@ -45,17 +49,16 @@ export default function Home({products}: ProductsProps) {
       className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <header>
-        <h1>Header</h1>
+        Header
       </header>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-      <h1>Main</h1>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          {process.env.NEXT_PUBLIC_API_URL}
+      <h1>Products list</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          {productslist}
         </div>
-        {productslist}
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-          <h1>Footer</h1>
+          Footer
       </footer>
     </div>
   );
--- src/pages/product/[product].tsx
+++ src/pages/product/[product].tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-
+import { Geist, Geist_Mono } from "next/font/google";
+import Link from 'next/link';
 
 export type ProductProps = {
     id: number,
@@ -13,6 +14,16 @@ export type ProductProps = {
     title: string
 }
 
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
 export async function getStaticPaths() {
   const paths:any = [];
   const getStaticPathsObj = {
@@ -23,7 +34,7 @@ export async function getStaticPaths() {
   const res = await fetch(process.env.NEXT_PUBLIC_API_URL || '')
   const products = await res.json()
   
-  products.forEach((product)=>{
+  products.forEach((product:any)=>{
 
       getStaticPathsObj.paths.push(
           {
@@ -35,7 +46,7 @@ export async function getStaticPaths() {
   return getStaticPathsObj;
 }
 
-export async function getStaticProps<ProductProps>(context) {
+export async function getStaticProps<ProductProps>(context: any) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL || ''}${context.params.product}`)
   const product = await res.json()
 
@@ -45,17 +56,32 @@ export async function getStaticProps<ProductProps>(context) {
 }
 
 export default function Products({id, brand, category, description, images, price, stock, thumbnail, title}:ProductProps) {
+
   return (
-    <div>
-      {id}
-      {brand} 
-      {category} 
-      {description}
-      {images}
-      {price}
-      {stock}
-      {thumbnail}
-      {title}
+    <div
+      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    >
+      <header>
+        Header
+      </header>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div>
+          <Link href="/">&lt;&nbsp;Back to product list</Link>
+        </div>
+        <div className="border rounded-lg p-4">
+          <div className="justify-items-center">
+            <Image src={images[0]} alt={title} width="400" height="400"/>
+          </div>
+          <div className=""><h1>{title}</h1></div>
+          <div className="font-bold text-sm capitalize">{category}</div>
+          <div className="">{stock} in stock</div>
+          <div className="font-bold">£{price}</div>
+          <div>{description}</div>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+          Footer
+      </footer>
     </div>
   );
 }
